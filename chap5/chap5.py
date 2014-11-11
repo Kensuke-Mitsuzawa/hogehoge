82eb5fb1e2f660c64b36c9bf849c56b391e7665d
kensuke-mi <kensuke-mi@is.naist.jp>
2014-11-11 21:35:25 +0900

    よくわからなくなったので、とりあえず

--- chap5/chap5.py
+++ chap5/chap5.py
@@ -42,10 +42,38 @@ def SummariseClickRate(ab_test_imp):
     cvr_result = ab_test_imp.groupby('test_case_x').apply(Calc_func)
     result_map = stats.chi2_contingency(cvr_result)
     print u'-----------------------------'
-    print u'p値は{}'.format(result_map[0])
+    #print u'p値は{}'.format(result_map[0])
+
+
+def Calc_func2(x):
+    N_click_user = len(x.is_goal[x.is_goal == 1])
+    cvr = float(N_click_user) / len(x.user_id_x)
+
+
+def SummarisePerdate(ab_test_imp):
+    # ab_test_imp_summary = ab_test_imp.groupby(
+    #    ['log_date_x', 'test_case_x']).agg(
+    #        {'user_id_x': {'imp': 'size'},
+    #         'is_goal': {'cv': 'sum'},
+    #         'aa': 'mean'})
+    ab_test_imp_summary = ab_test_imp.groupby(
+        ['log_date_x', 'test_case_x'])[['user_id_x', 'is_goal']].apply(Calc_func)
+    ab_test_imp_summary = pd.DataFrame(ab_test_imp_summary).reset_index()
+    
+    ab_test_imp_sum_length_summary = ab_test_imp.groupby(
+        ['log_date_x', 'test_case_x']).agg(
+            {'user_id_x': {'imp': 'size'},
+             'is_goal': {'cv': 'sum'}})
+    ab_test_imp_sum_length_summary = pd.DataFrame(ab_test_imp_sum_length_summary).reset_index()
+
+    ab_test_imp_sum_length_summary.columns = ['log_date_x', 'test_case_x', 'cv', 'imp']
+    Calc_func2 = lambda x: float(sum(x.cv)) / sum(x.imp)
+    ab_test_imp_summary = ab_test_imp_sum_length_summary.groupby('test_case_x').apply(Calc_func2)
+    print ab_test_imp_summary
 
 if __name__ == '__main__':
     ab_test_goal, ab_test_imp = LoadData()
     ab_test_imp = MergeData(ab_test_goal, ab_test_imp)
     ab_test_imp = MakeNewFlag(ab_test_imp)
     SummariseClickRate(ab_test_imp)
+    SummarisePerdate(ab_test_imp)
