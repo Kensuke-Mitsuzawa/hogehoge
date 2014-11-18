6e489134eeda49b6cbc4d79570194cf758388517
kensuke-mi <kensuke-mi@is.naist.jp>
2014-11-19 08:42:49 +0900

    plotを追加

--- chap3/chap3.py
+++ chap3/chap3.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 def Page53():
     dau = pd.read_csv('../sample_data/section3-dau.csv', sep=',')
@@ -59,11 +60,40 @@ def Page60_61(mau_payment):
     return mau_payment_summary
 
 
-def PlotGraph(mau_payment_summary):
-    df_bar = mau_payment_summary.ix[:, ['log_month', 'payment', 'user_type']]
-    payment_bar = df_bar.plot(kind='bar', stacked=True)
-    bar_fig = payment_bar.get_figure()
-    bar_fig.savefig('./payment_bar.png')
+def PlotGraph(mau_payment_summary, mau_payment):
+    #df_bar = mau_payment_summary.ix[:, ['log_month', 'payment', 'user_type']]
+    #payment_bar = df_bar.plot(kind='bar', stacked=True)
+    #bar_fig = payment_bar.get_figure()
+    #bar_fig.savefig('./payment_bar.png')
+
+    # matplotlibだと、次の様に書く
+    w = 0.4
+    # existingとinstallを別のグループにして棒グループを描く
+    Y1 = mau_payment_summary.ix[mau_payment_summary.user_type == 'existing', 'payment']
+    Y2 = mau_payment_summary.ix[mau_payment_summary.user_type == 'install', 'payment']
+    X = np.arange(len(Y1))
+
+    fig = plt.figure()
+    fig_bar = fig.add_subplot(111)
+    plt.xticks(X, ['2013-06', '2013-07'])
+    fig_bar.bar(X, Y1, color='b', width=w, label='existing', align='center')
+    fig_bar.bar(X, Y2, color='g', width=w, bottom=Y1, label='existing', align='center')
+    fig_bar.grid(True)
+    plt.show()
+    plt.savefig('./bar_sum.png')
+
+    # グループ化して、棒グラフをプロットする
+    df_install_above = mau_payment[(mau_payment.payment > 0) & (mau_payment.user_type == 'install')]
+    df_06 = df_install_above.ix[df_install_above.log_month == '2013-06', 'payment']
+    df_07 = df_install_above.ix[df_install_above.log_month == '2013-07', 'payment']
+    df_06.hist(label='2014-06')
+    df_07.hist(label='2014-07')
+    plt.xlabel('payment')
+    plt.ylabel('count')
+    plt.savefig('./hist_payment.png')
+    plt.show()
+
+
 
 def Main():
     dau, install, dpu = Page53()
@@ -71,7 +101,7 @@ def Main():
     mau_payment = Page58(dau_install_payment)
     mau_payment_summary = Page60_61(mau_payment)
     print mau_payment_summary
-    PlotGraph(mau_payment_summary)
+    PlotGraph(mau_payment_summary, mau_payment)
 
 if __name__ == '__main__':
     Main()
