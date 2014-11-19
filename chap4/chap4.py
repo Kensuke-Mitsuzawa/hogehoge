3bf22cbe12c3abfc0a324d83fe7a9e093129e21a
kensuke-mi <kensuke-mi@is.naist.jp>
2014-11-19 09:01:18 +0900

    ４章のグラフ追加

--- chap4/chap4.py
+++ chap4/chap4.py
@@ -2,7 +2,8 @@
 # -*- coding:utf-8 -*-
 __author__ = 'Kensuke Mitsuzawa'
 import pandas as pd
-
+import matplotlib.pyplot as plt
+from datetime import datetime as dt
 
 def LoadCSV():
     dau = pd.read_csv('../sample_data/section4-dau.csv', sep=",")    
@@ -36,11 +37,22 @@ def SegmentSummarise(dau_user_info):
     print(pd.crosstab(dau_user_info['log_month'], dau_user_info['device_type']))
 
 
-def PlotResult():
+def PlotResult(dau_user_info):
     """
     結果を時系列データでプロットする
     """
+    dau_user_info_device_summary = dau_user_info.groupby(
+        ['log_date', 'device_type']).apply(lambda x: len(x.user_id)).reset_index()
+    # pandasを用いたグラフでグループ化する場合は、列ごとにグループ化する必要がある
+    graph_input = pd.pivot_table(dau_user_info_device_summary,
+                                 index='log_date',
+                                 columns='device_type', values=0).reset_index()
+    graph_input['log_date'] = graph_input['log_date'].apply(lambda x: dt.strptime(x, '%Y-%m-%d'))
 
+    plt.figure()
+    graph_input.plot()
+    plt.show()
+    plt.savefig('./dau_user_summary.png')
 
 if __name__ == '__main__':
     dau, user_info = LoadCSV()
